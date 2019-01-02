a86c62c2b323fa83af9d506212897d2d17ac4900
Josef Schmeißer <josef.schmeisser@tum.de>
2019-01-02 15:16:57 +0100

    remove unused files

--- dagger_mn_nat/train.py
+++ dagger_mn_nat/train.py
@@ -10,103 +10,49 @@ from os import path
 from subprocess import Popen, call
 from helpers.helpers import get_open_udp_port
 
+from helpers.config import *
 
-def run(args):
-    # run worker.py on ps and worker hosts
-    for job_name in ['ps', 'worker']:
-        host_list = args[job_name + '_list']
-        procs = args[job_name + '_procs']
+import mininet_topo
 
-        for i in xrange(len(host_list)):
-            ssh_cmd = ['ssh', host_list[i]]
+def run():
+    section = config.get_our_section()
+    role_type = section['role_type']
+    if role_type == 'ps':
+        ps_section = config.get_our_section()
 
-            cmd = ['perl', args['worker_src'],
-                   '--ps-hosts', args['ps_hosts'],
-                   '--worker-hosts', args['worker_hosts'],
-                   '--job-name', job_name,
-                   '--task-index', str(i)]
+        worker_hosts_arg = ','.join([worker['address'] for worker in get_full_worker_list()])
+#        print('worker_hosts_arg:', worker_hosts_arg)
+        ps_hosts_arg = ','.join([ps['address'] for ps in get_ps_list()])
+#        print('ps_hosts_arg:', ps_hosts_arg)
 
-            cmd = ssh_cmd + cmd
+        cmd = ['perl', 'worker.py',
+                '--ps-hosts', ps_hosts_arg,
+                '--worker-hosts', worker_hosts_arg,
+                '--job-name', 'ps',
+                '--task-index', str(ps_section['task_index'])]
+        sys.stderr.write('$ %s\n' % ' '.join(cmd))
 
-            sys.stderr.write('$ %s\n' % ' '.join(cmd))
-            procs.append(Popen(cmd, preexec_fn=os.setsid))
-
-    # ps will block forever
-    for ps_proc in args['ps_procs']:
-        ps_proc.communicate()
-
-
-def cleanup(args):
-    all_procs = args['ps_procs'] + args['worker_procs']
-    for proc in all_procs:
+        p = Popen(cmd, preexec_fn=os.setsid)
         try:
-            os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
-        except OSError as e:
-            sys.stderr.write('%s\n' % e)
+            p.wait()
+        except KeyboardInterrupt:
+            p.terminate()
 
-    host_set = set(args['ps_list'] + args['worker_list'])
-    pkill_script = path.join(args['rlcc_dir'], 'helpers', 'pkill.py')
-
-    for host in host_set:
-        kill_cmd = ['ssh', host, 'perl', pkill_script, args['rlcc_dir']]
-        sys.stderr.write('$ %s\n' % ' '.join(kill_cmd))
-        call(kill_cmd)
-
-    sys.stderr.write('\nAll cleaned up.\n')
-
-
-def construct_args(prog_args):
-    # construct a dictionary of arguments
-    args = {}
-
-    # file paths
-    args['rlcc_dir'] = prog_args.rlcc_dir
-    args['worker_src'] = path.join(args['rlcc_dir'], 'dagger', 'worker.py')
-
-    # hostnames and processes
-    args['ps_hosts'] = prog_args.ps_hosts
-    args['worker_hosts'] = prog_args.worker_hosts
-
-    args['ps_list'] = prog_args.ps_hosts.split(',')
-    args['worker_list'] = prog_args.worker_hosts.split(',')
-    args['username'] = prog_args.username
-
-    for i, host in enumerate(args['ps_list']):
-        args['ps_list'][i] = args['username'] + '@' + host.split(':')[0]
-
-    for i, host in enumerate(args['worker_list']):
-        args['worker_list'][i] = args['username'] + '@' + host.split(':')[0]
-
-    args['ps_procs'] = []
-    args['worker_procs'] = []
-
-    return args
+#        p.wait()
+#        call(cmd)
+    elif role_type == 'mn':
+        controller = mininet_topo.Controller()
+        controller.run()
+    else:
+        sys.stderr.write('unknown role_type: %s\n' % role_type)
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--ps-hosts', required=True, metavar='[HOSTNAME:PORT, ...]',
-        help='comma-separated list of hostname:port of parameter servers')
-    parser.add_argument(
-        '--worker-hosts', required=True, metavar='[HOSTNAME:PORT, ...]',
-        help='comma-separated list of hostname:port of workers')
-    parser.add_argument(
-        '--username', default='ubuntu',
-        help='username used in ssh connection (default: ubuntu)')
-    parser.add_argument(
-        '--rlcc-dir', metavar='DIR', default='/home/ubuntu/RLCC',
-        help='absolute path to RLCC/ (default: /home/ubuntu/RLCC)')
-    prog_args = parser.parse_args()
-    args = construct_args(prog_args)
-
-    # run worker.py on ps and worker hosts
-    try:
-        run(args)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        cleanup(args)
+    parse_config_args()
+#    try:
+    run()
+#    except KeyboardInterrupt:
+#        pass
 
 
 if __name__ == '__main__':
